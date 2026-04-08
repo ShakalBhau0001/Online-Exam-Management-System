63ae9734f788095259f3914b30a1bd1a7d76b2a2
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-04-08 11:47:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import customtkinter as ctk
+from gui.login import LoginWindow
 
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
+if __name__ == "__main__":
+    app = LoginWindow()
+    app.mainloop()
